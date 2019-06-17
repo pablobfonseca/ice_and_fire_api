e9591277626205026705e8179d8c49041dea2c74
Pablo Fonseca <pablofonseca777@gmail.com>
2019-06-16 23:58:34 -0300

    Includes FactoryBot::Syntax::Methods to rails_helper

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Book, type: :model do
   describe '#as_json' do
     it 'returns the formatted json representation' do
-      book = FactoryBot.create(:book)
+      book = create(:book)
       json = book.to_json(status_code: 201, status: "success")
 
       expect(JSON.parse(json, symbolize_names: true))
